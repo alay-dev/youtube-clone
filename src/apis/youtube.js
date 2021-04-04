cc959e346a5a14fb9e57b79537bc2b0bbcaa1d79
alay-dev <78652000+alay-dev@users.noreply.github.com>
2021-04-04 12:37:38 +0530

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,6 @@
 import axios from 'axios' ;
 
-const KEY = 'AIzaSyBA2dSxHzmPm32Jm0Z5oqJM1HcSLtXNG7c' ;
-// const KEY = 'AIzaSyDj_wb2R-l3eTD6Y5p1V42ixd8fJ_ZHT3o' ;
+const KEY = <YOUR YOUTUBE API KEY> ;
 
 export const search = axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
